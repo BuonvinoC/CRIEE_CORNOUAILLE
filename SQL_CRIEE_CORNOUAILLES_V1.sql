9bdb038a0ce0f301abd8208bb1e9a4b22e8ce4d5
Zimsacha <43068491+Zimsacha@users.noreply.github.com>
2019-03-12 11:34:25 +0100

    Update SQL_CRIEE_CORNOUAILLES_V1.sql

--- SQL_CRIEE_CORNOUAILLES_V1.sql
+++ SQL_CRIEE_CORNOUAILLES_V1.sql
@@ -144,6 +144,19 @@ ALTER TABLE `panier_intermedaire`
   ADD PRIMARY KEY (`mailAcheteur`,`idLot`),
   ADD KEY `FK_PANINER_INTER_2` (`idLot`);
 
+
+INSERT INTO ESPECE
+VALUES
+(1, 'Cabillaud', 'Gadus morhua', 'cabillaud.png'),
+(2, 'Carpe', 'Cyprinus carpio', 'carpe.png'),
+(3, 'Hareng', 'Clupea harengus', 'hareng.png'),
+(4, 'Maquereau', ' Scomber scombrus', 'maquereau.png'),
+(5, 'Sardine', 'Sardina pilchardus', 'sardine.png'),
+(6, 'Saumon', 'Salmo Salar', 'saumon.png'),
+(7, 'Sole', 'Solea solea', 'sole.png'),
+(8, 'Thon', 'Thunnus thynnus', 'thon.png'),
+(9, 'Truite', 'Salmo trutta', 'truite.png'),
+(10, 'Turbot', 'Scophthalmus maximus', 'turbot.png');
 --
 -- Contraintes pour les tables déchargées
 --
