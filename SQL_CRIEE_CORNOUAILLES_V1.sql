3e710b981529fa6f93e782501d874286122ece86
BuonvinoC <39943584+BuonvinoC@users.noreply.github.com>
2019-03-15 14:55:07 +0100

    Update SQL_CRIEE_CORNOUAILLES_V1.sql

--- SQL_CRIEE_CORNOUAILLES_V1.sql
+++ SQL_CRIEE_CORNOUAILLES_V1.sql
@@ -143,18 +143,6 @@ VALUES
 --
 -- Contraintes pour les tables déchargées
 
-INSERT INTO ESPECE
-VALUES
-(1, 'Cabillaud', 'Gadus morhua', 'cabillaud.png'),
-(2, 'Carpe', 'Cyprinus carpio', 'carpe.png'),
-(3, 'Hareng', 'Clupea harengus', 'hareng.png'),
-(4, 'Maquereau', ' Scomber scombrus', 'maquereau.png'),
-(5, 'Sardine', 'Sardina pilchardus', 'sardine.png'),
-(6, 'Saumon', 'Salmo Salar', 'saumon.png'),
-(7, 'Sole', 'Solea solea', 'sole.png'),
-(8, 'Thon', 'Thunnus thynnus', 'thon.png'),
-(9, 'Truite', 'Salmo trutta', 'truite.png'),
-(10, 'Turbot', 'Scophthalmus maximus', 'turbot.png');
 
 --
 -- Contraintes pour la table `panier_intermedaire`
