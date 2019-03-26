94110ec160bb43574e7e39272bad6462edf316d3
BuonvinoC <39943584+BuonvinoC@users.noreply.github.com>
2019-03-26 09:23:31 +0100

    Update v_accueil.php

--- views/v_accueil.php
+++ views/v_accueil.php
@@ -1,4 +1,12 @@
-<?php 
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h2>Accueil</h2>
+	<img src="./../criee.jpg" width="50%">
+	<p>Bonjour et bienvenue sur notre boutique en ligne. Vous pourrez y découvrir la sélection de nos poissons dans notre catalogue. Nous vous invitons à vous inscrire et à profiter de nos articles. Veuillez nous contacter pour plus d'informations.</p>
+	<img src="./../poisson_avril.jpg">
+</body>
+</html>
