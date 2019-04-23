4d0cbfdd33bbbec0667ffb271080c0251c317406
BuonvinoC <39943584+BuonvinoC@users.noreply.github.com>
2019-04-23 10:54:18 +0200

    Update SQL_CRIEE_CORNOUAILLES_V1.sql

--- SQL_CRIEE_CORNOUAILLES_V1.sql
+++ SQL_CRIEE_CORNOUAILLES_V1.sql
@@ -3,9 +3,14 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  mar. 26 mars 2019 à 12:03
+-- Généré le :  mar. 12 mars 2019 à 10:25
 -- Version du serveur :  10.1.36-MariaDB
 -- Version de PHP :  7.2.10
+
+DROP DATABASE IF EXISTS CRIEE_CORNOUAILLES_V1;
+CREATE DATABASE IF NOT EXISTS CRIEE_CORNOUAILLES_V1;
+USE CRIEE_CORNOUAILLES_V1;
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +24,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `criee_cornouailles_v1`
+-- Base de données :  `crieev0`
 --
 
 -- --------------------------------------------------------
@@ -36,7 +41,19 @@ CREATE TABLE `acheteur` (
   `adresse` varchar(50) DEFAULT NULL,
   `ville` varchar(50) DEFAULT NULL,
   `codePostal` varchar(50) DEFAULT NULL
+ 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+CREATE TABLE `vendeur`(
+`mail` varchar(50) NOT NULL,
+`pwd` varchar(50) DEFAULT NULL,
+`prenom` varchar(50) DEFAULT NULL,
+`nom` varchar(50) DEFAULT NULL,
+`adresse` varchar(50) DEFAULT NULL,
+`ville` varchar(50) DEFAULT NULL,
+`codePostal` varchar(50) DEFAULT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -61,13 +78,14 @@ CREATE TABLE `lot` (
   `idLot` varchar(50) NOT NULL,
   `libelleLot` varchar(50) NOT NULL,
   `DatePeche` date NOT NULL,
-  `prixActuel` int(11) NOT NULL,
+  `prixActuel` int NOT NULL,
   `AcheteurMax` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `lot`
 --
+-- --------------------------------------------------------
 
 INSERT INTO `lot` (`idLot`, `libelleLot`, `DatePeche`, `prixActuel`, `AcheteurMax`) VALUES
 ('44', '', '0000-00-00', 300, '');
@@ -86,9 +104,8 @@ $$
 DELIMITER ;
 
 -- --------------------------------------------------------
-
 --
--- Structure de la table `panier_temporaire`
+-- Structure de la table `panier_intermedaire`
 --
 
 CREATE TABLE `panier_temporaire` (
@@ -96,33 +113,30 @@ CREATE TABLE `panier_temporaire` (
   `idLot` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+ALTER TABLE `panier_temporaire`
+  ADD PRIMARY KEY (`mailAcheteur`,`idLot`);
 
 --
--- Structure de la table `vendeur`
---
-
-CREATE TABLE `vendeur` (
-  `mail` varchar(50) NOT NULL,
-  `pwd` varchar(50) DEFAULT NULL,
-  `prenom` varchar(50) DEFAULT NULL,
-  `nom` varchar(50) DEFAULT NULL,
-  `adresse` varchar(50) DEFAULT NULL,
-  `ville` varchar(50) DEFAULT NULL,
-  `codePostal` varchar(50) DEFAULT NULL
+CREATE TABLE `encherir` (
+  `mailAcheteur` varchar(50) NOT NULL,
+  `idLot` varchar(50) NOT NULL,
+  `date_encherir`  datetime not null,
+  `prix_propose` float
+  
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Index pour les tables déchargées
---
+ALTER TABLE `encherir`
+  ADD constraint `pk_encherir` PRIMARY KEY (`mailAcheteur`,`idLot`, `date_encherir`);
+    
 
 --
 -- Index pour la table `acheteur`
 --
 ALTER TABLE `acheteur`
   ADD PRIMARY KEY (`mail`);
+  
+ALTER TABLE `vendeur`
+  ADD PRIMARY KEY (`mail`);
 
---
 -- Index pour la table `espece`
 --
 ALTER TABLE `espece`
@@ -134,29 +148,38 @@ ALTER TABLE `espece`
 ALTER TABLE `lot`
   ADD PRIMARY KEY (`idLot`);
 
+  ALTER TABLE `encherir`
+  ADD CONSTRAINT `FK_encherir_acheteur` FOREIGN KEY (`mailAcheteur`) REFERENCES `acheteur`(`mail`),
+	ADD CONSTRAINT `FK_encherir_lot` FOREIGN KEY (`idLot`) REFERENCES `lot`(`idLot`);
 --
--- Index pour la table `panier_temporaire`
---
+
 ALTER TABLE `panier_temporaire`
-  ADD PRIMARY KEY (`mailAcheteur`,`idLot`),
-  ADD KEY `idLot` (`idLot`);
+  ADD constraint FOREIGN KEY (`mailAcheteur`) REFERENCES `acheteur`(`mail`);
+ALTER TABLE `panier_temporaire`
+  ADD constraint FOREIGN KEY (`idLot`) REFERENCES `lot`(`idLot`);
 
---
--- Index pour la table `vendeur`
---
-ALTER TABLE `vendeur`
-  ADD PRIMARY KEY (`mail`);
 
+
+
+INSERT INTO ESPECE
+VALUES
+(1, 'Cabillaud', 'Gadus morhua', 'cabillaud.png'),
+(2, 'Carpe', 'Cyprinus carpio', 'carpe.png'),
+(3, 'Hareng', 'Clupea harengus', 'hareng.png'),
+(4, 'Maquereau', ' Scomber scombrus', 'maquereau.png'),
+(5, 'Sardine', 'Sardina pilchardus', 'sardine.png'),
+(6, 'Saumon', 'Salmo Salar', 'saumon.png'),
+(7, 'Sole', 'Solea solea', 'sole.png'),
+(8, 'Thon', 'Thunnus thynnus', 'thon.png'),
+(9, 'Truite', 'Salmo trutta', 'truite.png'),
+(10, 'Turbot', 'Scophthalmus maximus', 'turbot.png');
 --
 -- Contraintes pour les tables déchargées
---
+
 
 --
--- Contraintes pour la table `panier_temporaire`
+-- Contraintes pour la table `panier_intermedaire`
 --
-ALTER TABLE `panier_temporaire`
-  ADD CONSTRAINT `panier_temporaire_ibfk_1` FOREIGN KEY (`mailAcheteur`) REFERENCES `acheteur` (`mail`),
-  ADD CONSTRAINT `panier_temporaire_ibfk_2` FOREIGN KEY (`idLot`) REFERENCES `lot` (`idLot`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
