ce5754e8b4fba2e1fdd8fbfa255840b612b4b1a6
BuonvinoC <39943584+BuonvinoC@users.noreply.github.com>
2019-05-03 14:25:18 +0200

    Update v_enchere.php

--- views/v_enchere.php
+++ views/v_enchere.php
@@ -9,73 +9,7 @@
 		<th>Prix actuel</th>
 		<th>Acheteur Max</th>
 		<th>Enchérir</th>
-                <th>Temps restant</th>
-	</tr>
-				<?php
-				foreach ($donnees as $row) {?>
-	<tr>
-
-		<td><?php ?></td>
-    <td><p>120</p></td>
-		<td>100</td>
-
-
-		<td>
-			<?php
-				echo form_open('utilisateur/contenu/encherir');
-				$montant= array(
-				'name'=>'ajoutMontant',
-				'id'=>'ajoutMontant',
-				'placeholder'=>'Montant à ajouter',
-				"value"=>set_value('ajoutMontant')
-				);
-				echo form_input($montant);
-				echo form_submit('envoi','Ajouter');
-				echo form_close();
-			?>
-
-			<!-- <input type="text" name="ajoutPrix"><br/>
-			<input type="button" name="valider" value="Valider" onclick="window.location.reload()"><br/> -->
-		</td>
-                <td class="Timer">
-                    <script> setInterval(function() { makeTimer($); }, 1000);</script>
-                    <script>
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
-// Update the count down every 1 second
-var x = setInterval(function() {
-  // Get todays date and time
-  var now = new Date().getTime();
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementByClassName("Timer").innerHTML = "EXPIRED";
-  }
-}, 1000);
-</script>
-                </td>
-
-	</tr>
-				<?php  } ?>
-</table>
-
-<table>
-	<tr>
-		<th>Reference Lot</th>
-		<th>Prix actuel</th>
-		<th>Acheteur Max</th>
-		<th>Enchérir</th>
-                <th>Temps restant</th>
+        <th>Temps restant</th>
 	</tr>
 				<?php
 				foreach ($donnees as $row) {?>
@@ -93,7 +27,7 @@ var x = setInterval(function() {
 				'name'=>'ajoutMontant',
 				'id'=>'ajoutMontant',
 				'placeholder'=>'Montant à ajouter',
-				'value'=>set_value('ajoutMontant')
+				"value"=>set_value('ajoutMontant')
 				);
 				echo form_input($montant);
 				echo form_submit('envoi','Ajouter');
@@ -103,35 +37,51 @@ var x = setInterval(function() {
 			<!-- <input type="text" name="ajoutPrix"><br/>
 			<input type="button" name="valider" value="Valider" onclick="window.location.reload()"><br/> -->
 		</td>
+		<td><?php
+		date_default_timezone_set('Europe/Paris');
+
+		$annee = date('Y');
+		$mois = 5;
+		$jour = 30;
+		$heure = 12;
+		$minute = 50;
+		$seconde = 0;
+		// Heure, minute, seconde, mois, jour
+		// $heureFinEnchere = mktime(10, 50, 0, 5, 30, $annee);
+		$heureFinEnchere = mktime($heure, $minute, $seconde, $mois, $jour, $annee);
+
+
+		$tps_restant = $heureFinEnchere - time();
+
+		 if ($heureFinEnchere < time())
+		 $heureDebutEnchere = mktime($heure, $minute, $seconde, $mois, $jour, ++$annee);
+
+		 $tps_restant = $heureFinEnchere - time(); 
+
+		//============ CONVERSIONS
+
+		$i_restantes = $tps_restant / 60;
+		$H_restantes = $i_restantes / 60;
+		$d_restants = $H_restantes / 24;
+
+
+		$s_restantes = floor($tps_restant % 60); // Secondes restantes
+		$i_restantes = floor($i_restantes % 60); // Minutes restantes
+		$H_restantes = floor($H_restantes % 24); // Heures restantes
+		$d_restants = floor($d_restants); // Jours restants
+		//==================
+
+		setlocale(LC_ALL, 'fr_FR');
+
+		echo 
+
+		   '</strong> <strong>' . $d_restants .'J </strong> <strong>'. $H_restantes .'H </strong>'
+		   . ' <strong>'. $i_restantes .'MIN </strong> et <strong>'. $s_restantes .'s</strong>';
+
+		?></td>
 
 	</tr>
-				<?php  } ?>
+				<?php   } ?>
         
-<script> 
- var start = new Date;
-function makeTimer(endDate) {
-	//		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");	
-		var endTime = new Date("29 April 2020 9:56:00 GMT+01:00");			
-			endTime = (Date.parse(endTime) / 1000);
-                        
-                        endDate = (Date.parse(endDate) / 1000);
-			var now = new Date();
-			now = (Date.parse(now) / 1000);
-			var timeLeft = endTime - now;
-			var days = Math.floor(timeLeft / 86400); 
-			var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-			var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-			var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
-  
-			if (hours < "10") { hours = "0" + hours; }
-			if (minutes < "10") { minutes = "0" + minutes; }
-			if (seconds < "10") { seconds = "0" + seconds; }
-			$("#days").html(days + "<span>Days</span>");
-			$("#hours").html(hours + "<span>Hours</span>");
-			$("#minutes").html(minutes + "<span>Minutes</span>");
-			$("#seconds").html(seconds + "<span>Seconds</span>");		
-	}
-</script>
-
 
 </table>
