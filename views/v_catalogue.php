7457bc614b462cb50c335023d94b1cf590238fa9
Zimsacha <43068491+Zimsacha@users.noreply.github.com>
2019-05-03 13:15:06 +0200

    Update v_catalogue.php

--- views/v_catalogue.php
+++ views/v_catalogue.php
@@ -1,12 +1,25 @@
 <head>
 <link href="<?php echo base_url('css/catalogue.css');?>" rel="stylesheet" type="text/css">
 </head>
+
+
 <table>
 	<tr>
 		<th>Photo</th>
 		<th>Reference Lot</th>
 		<th>Nom</th>
 		<th>Nom Scientifique</th>
+                <th>
+                    <?php
+                        if ($this->session->userdata('login')=="admin"){                           
+                        echo "Lancer enchère";
+                        }
+                        else {
+                        echo "Ce poisson m'interesse"; }
+                            ?>
+                        
+                        
+                </th>
 	</tr>
 				<?php
 				foreach ($donnees as $row) {?>
@@ -16,7 +29,31 @@
 		<td><?php echo $row['idEspece'];?></td>
 		<td><?php echo $row['nomEsp'];?></td>
 		<td><?php echo $row['nomSciEsp'];?></td>
+                <td>
+                    <?php
+                        if ($this->session->userdata('login')=="admin"){
+                            echo form_open('utilisateur/debuter_enchere/');        
+                            $data = array(
+                                    'name'=> 'enchere[]',
+                                    'id'=> 'enchere',
+                                    'value'=>$row['idEspece']
+                            );
+                            echo form_checkbox($data);
+                                            }
+                        else {
+                            echo form_open('utilisateur/debuter_enchere/');        
+                            $data = array(
+                                    'name'=> 'enchere[]',
+                                    'id'=> 'enchere',
+                                    'value'=>$row['idEspece']
+                            );
+                            echo form_checkbox($data);
+                                                    }
+                            ?>
+                </td>
 
 	</tr>
 				<?php  } ?>
 </table>
+
+<?php echo form_submit("go","go") ;?>
