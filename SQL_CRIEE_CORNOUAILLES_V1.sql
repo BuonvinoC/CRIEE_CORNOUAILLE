889a10dec7427faf4d84e2f7b71718c44ab2e63b
Zimsacha <43068491+Zimsacha@users.noreply.github.com>
2019-05-04 16:04:07 +0200

    Update SQL_CRIEE_CORNOUAILLES_V1.sql

--- SQL_CRIEE_CORNOUAILLES_V1.sql
+++ SQL_CRIEE_CORNOUAILLES_V1.sql
@@ -1,20 +1,32 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7.1
--- http://www.phpmyadmin.net
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
 --
--- Client :  localhost
--- Généré le :  Ven 03 Mai 2019 à 12:57
--- Version du serveur :  5.6.20-log
--- Version de PHP :  5.4.31
+-- Hôte : 127.0.0.1
+-- Généré le :  mar. 12 mars 2019 à 10:25
+-- Version du serveur :  10.1.36-MariaDB
+-- Version de PHP :  7.2.10
+
+DROP DATABASE IF EXISTS CRIEE_CORNOUAILLES_V1;
+CREATE DATABASE IF NOT EXISTS CRIEE_CORNOUAILLES_V1;
+USE CRIEE_CORNOUAILLES_V1;
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `crieev0`
+--
+
 
 --
 -- Base de données :  `criee_cornouailles_v1`
@@ -41,7 +53,7 @@ CREATE TABLE IF NOT EXISTS `acheteur` (
 --
 
 INSERT INTO `acheteur` (`mail`, `pwd`, `prenom`, `nom`, `adresse`, `ville`, `codePostal`) VALUES
-('Buonvino.clement@gmail.com', '11061997', 'Clément', 'Buonvino', NULL, NULL, NULL);
+('Buonvino.clement@gmail.com', 'xi0k9mjz', 'Clément', 'Buonvino', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -105,7 +117,7 @@ CREATE TABLE IF NOT EXISTS `lot` (
 --
 
 INSERT INTO `lot` (`idLot`, `libelleLot`, `DatePeche`, `prixActuel`, `AcheteurMax`, `dateFinEnchere`) VALUES
-('1', 'libelleLot', '2019-05-14', 500, 'Buonvino.clement@gmail.com', '2019-05-30 10:30:00');
+('1', 'libelleLot', '2019-05-14', 500, 'Buonvino.clement@gmail.com', '0000-00-00 00:00:00');
 
 --
 -- Déclencheurs `lot`
