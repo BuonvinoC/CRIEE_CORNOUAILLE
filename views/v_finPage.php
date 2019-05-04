fc4827e6368e765f2a02297c214f330638c8e5d8
MDeghaud67 <43568436+MDeghaud67@users.noreply.github.com>
2019-05-04 17:53:49 +0200

    Update v_finPage.php

--- views/v_finPage.php
+++ views/v_finPage.php
@@ -1,3 +1,16 @@
-<div id="finPage">
+<head>
+    <meta charset="utf-8">
+<link href="<?php echo base_url('css/finPage.css');?>" rel="stylesheet" type="text/css">
+</head>
+<div id="finPage" class="footer">
+    <table align="center">
+        <td><img src="./../images/paiement.png" width="50%" align="right"></td>
+        <td><img src="./../images/livraison48.jpg" width="50%" align="left"></td>
+    </table>
+    <table align="center">
+        <td><a href="">Contact</a></td>
+        <td><a href="">Mentions légales</a></td>
+        <td><a href="">FAQ</a></td>
+    </table>
 <p><a href="">Copyright...</a></p>
 </div>
