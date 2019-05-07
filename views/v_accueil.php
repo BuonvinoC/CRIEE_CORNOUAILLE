f6f9b4471b3a557a57258c3c69b05b486992757d
MDeghaud67 <43568436+MDeghaud67@users.noreply.github.com>
2019-05-07 11:10:14 +0200

    Update v_accueil.php

--- views/v_accueil.php
+++ views/v_accueil.php
@@ -2,25 +2,26 @@
 <html>
 <head>
 	<meta charset="utf-8">
-    <link style="css">
+    <link style="css" href="<?php echo base_url('css/style.css');?>" rel="stylesheet">
 </head>
 <body>
-	<h2>Accueil</h2>
-	<img src="./../images/la_criee.jpg" width="50%">
-	<p>Bonjour et bienvenue sur notre boutique en ligne. Vous pourrez y découvrir la sélection de nos poissons dans notre catalogue. Nous vous invitons à vous inscrire et à profiter de nos articles. Veuillez nous contacter pour plus d'informations.</p>
+	<img src="./../images/la_criee.jpg" width="50%" align="left">
     <table align="center">
-        <td><a href="<?php echo site_url('utilisateur/contenu/catalogue');?>"><img src="./../images/nos_produits.png" align="left"></a></td>
-        <td><a href="<?php echo site_url('utilisateur/contenu/enchere');?>"><img src="./../images/img_encheres.jpg" align="right"></a></td>
-    </table>
+        <tr><td><a href="<?php echo site_url('utilisateur/contenu/catalogue');?>"><img src="./../images/nos_produits.png" align="left"/></a></td></tr>
+        <tr><td><a href="<?php echo site_url('utilisateur/contenu/enchere');?>"><img src="./../images/img_encheres.jpg" align="right"></a></td></tr></table>
+    <img src="./../images/poisson_avril.jpg">
+    <p>Bonjour et bienvenue sur notre boutique en ligne. Vous pourrez y découvrir la sélection de nos poissons dans notre catalogue. Nous vous invitons à vous inscrire et à profiter de nos articles. Veuillez nous contacter pour plus d'informations.</p>
     <h1>Qu'est-ce que la Criée de Cournaille ?</h1>
     <br>
-    <p>La Criée de Cournaille est une vente aux enchères d'une marchandise par lots. situées dans la commune de Plouhinec, département du Finistère (29), à proximité de la Pointe du Razsur la zone de pêche Atlantique du Nord Est.</p>
+    <p>La Criée de Cournouaille est une vente aux enchères d'une marchandise par lots. Notre entreprise est située dans la commune de Plouhinec, département du Finistère (29), à proximité de la Pointe du Razsur la zone de pêche Atlantique du Nord Est.</p>
     <table>
         <td><img src="./../images/localisation_criee.jpg" width="60%"></td>
         <td><img src="./../images/loc_criee.jpg" width="90%"></td>
     </table>
-    
-    <p>La marchandise vendue est le produit de la pêche, c'est-à-dire des poissons. Les vendeurs sont aussi des pêcheurs, les acheteurs sont en général des marreyeurs, mais ils peuvent être également des représentants de la grande distribution, des restaurateurs...</p>
+    <p>La marchandise vendue est le produit de la pêche, c'est-à-dire des poissons. Les vendeurs sont aussi des pêcheurs, mais peuvent être également des représentants de la grande distribution, des restaurateurs etc...</p>
+    <p>Nos clients sont des acheteurs particuliers, des restaurateurs, des poissonniers et même des entreprises.</p>
     <p>Les enchères descendent tant qu'aucun acheteur n'est preneur de la marchandise. Dans le cas contraire, le lot est cédé à une entreprise d'équarrissage.</p>
+    <h1>Notre objectif</h1>
+    <p>Nous voulons nous assurer que vous trouvez les produits que vous désirez et que vous puissez effectuer vos achats plus facilement en vous permettant des commandes en ligne par transaction bancaire et une livraison rapide directement à votre domicile.</p>
 </body>
 </html>
