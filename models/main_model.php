8dce00be89a3e457ac15bfbaa9f7f248815e0766
BuonvinoC <39943584+BuonvinoC@users.noreply.github.com>
2019-05-09 17:14:04 +0200

    Update main_model.php

--- models/main_model.php
+++ models/main_model.php
@@ -16,7 +16,6 @@ class Main_model extends CI_Model{
 		return $result;
 		$this->db=null;
 		}
-
 	
 	public function afficheProduits1() {
 		$this->load->database();
@@ -31,16 +30,12 @@ class Main_model extends CI_Model{
 	public function afficheProduits2() {
 		$this->load->database();
 		$sqql = $this->db->conn_id->prepare("SELECT * FROM LOT WHERE LOT.idLot NOT IN (SELECT idLot FROM lot_remporté)");
-
                  //SELECT ESPECE.LibelleEspece, ESPECE.Image, LOT.Qtt, LOT.Code, LOT.Libelle FROM LOT, ESPECE WHERE Lot.idEspece = Espece.idEspece and LOT.AcheteurMax NOT NULL
 		$sqql->execute();
 		$donnees = $sqql->fetchAll();
 		$this->db=null;
 		return $donnees;
 	}
-
-
-
 	public function InsertPanier($designation, $quantite) {// fonction d'insertion dans la base de données DONNEES
 		$this->load->database();
 		$req = $this->db->conn_id->prepare('INSERT INTO PANIER(designationProduit, quantite) VALUES (:designation, :quantite)');
@@ -89,7 +84,6 @@ class Main_model extends CI_Model{
 		{
                     if (($data['mail']=="admin")&&($data['pwd']=="admin"))
 				$session=2;
-
                     if (($row['mail']==$data['mail'])&&($row['pwd']==$data['pwd']))
 				$session=1;
 		}
@@ -114,9 +108,7 @@ class Main_model extends CI_Model{
 			$this->load->helper('url_helper');
 			$this->load->view('v_entete');
 			$this->load->view('v_bandeau');
-
                 }
-
                 else{
 			$this->load->helper('url_helper');
 			$this->load->view('v_entete');
@@ -125,10 +117,8 @@ class Main_model extends CI_Model{
 			$this->load->view('v_connexion');
 		}
 	}
-
          public function insertLotPropose($lbl,$poi,$date) {// fonction d'insertion dans la base de données DONNEES
 			$this->load->database();
-
                             $req = $this->db->conn_id->prepare('INSERT INTO lot_proposé (libelleLot, poisson, datePeche ) VALUES (:lbl, :poi, :date )');
                             $req->bindParam('lbl', $lbl, PDO::PARAM_STR); // on associe chaque paramètres
                             $req->bindParam('poi', $poi, PDO::PARAM_STR); // on associe chaque paramètres
@@ -136,21 +126,16 @@ class Main_model extends CI_Model{
                             $result = $req->execute();
                             return $result;
                             $this->db=null;
-
 	}
-
         public function insertLotValide($prix,$date) {// fonction d'insertion dans la base de données DONNEES
 			$this->load->database();
-
                             $req = $this->db->conn_id->prepare('INSERT INTO lot (prixActuel, DateFinEnchère ) VALUES (:prix, :date )');
                             $req->bindParam('prix', $prix, PDO::PARAM_STR); // on associe chaque paramètres
                             $req->bindParam('date', $date, PDO::PARAM_STR); // on associe chaque paramètres
                             $result = $req->execute();
                             return $result;
                             $this->db=null;
-
 	}
-
         public function ajoutEnchere($data) {// fonction d'insertion dans la base de données DONNEES
 			$this->load->database();
                         foreach ($data as $poisson ){
@@ -162,18 +147,15 @@ class Main_model extends CI_Model{
                             $this->db=null;
                         }
 	}
-
        public function afficheLotPropose() {
 		$this->load->database();
 		$sqql = $this->db->conn_id->prepare("SELECT * FROM lot_proposé");
-
                  //SELECT ESPECE.LibelleEspece, ESPECE.Image, LOT.Qtt, LOT.Code, LOT.Libelle FROM LOT, ESPECE WHERE Lot.idEspece = Espece.idEspece and LOT.AcheteurMax NOT NULL
 		$sqql->execute();
 		$donnees = $sqql->fetchAll();
 		$this->db=null;
 		return $donnees;
 	}
-
 	public function InsertLot($prx, $dat, $lbl, $datP) {// fonction d'insertion dans la base de données DONNEES
 		$this->load->database();
 		$req = $this->db->conn_id->prepare('INSERT INTO lot(libelleLot, DatePeche, prixActuel, dateFinEnchere) VALUES (:lbl, :datP, :prx, :dat)');
@@ -185,19 +167,15 @@ class Main_model extends CI_Model{
 		return $result;
 		$this->db=null;
 	}
-
 public function lotRemporte($idLot/*, $prix, $acht*/) {
 		$this->load->database();
-
 		$req = $this->db->conn_id->prepare("INSERT INTO lot_remporté(idLot) VALUES (:idL)");
 		$req->bindParam('idL', $idLot, PDO::PARAM_STR);
-
 		$result = $req->execute();
 		return $result;
 		$this->db=null;
 	/*	$this->load->database();
         echo ($libl. " " .$prix. " " .$acht);
-
         $pdo = new PDO("mysql:host=localhost;dbname=criee_cornouailles_v1","root","");
         $req = 'SELECT libelleLot FROM LOT WHERE idLot = 1';
         $stmt = $pdo->prepare($req);
@@ -206,9 +184,13 @@ public function lotRemporte($idLot/*, $prix, $acht*/) {
         return $stmt;
         $this->db=null;*/
 	}
-
-
-
-
+	public function afficheLotRemporte() {
+		$this->load->database();
+		$sqql = $this->db->conn_id->prepare("SELECT * FROM LOT WHERE LOT.idLot NOT IN (SELECT idLot FROM lot_remporté)");
+		$sqql->execute();
+		$donnees = $sqql->fetchAll();
+		$this->db=null;
+		return $donnees;
+	}
 }
 ?>
