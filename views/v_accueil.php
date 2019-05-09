3f720c08ae5785a2756679464222fbda6ac3e838
MDeghaud67 <43568436+MDeghaud67@users.noreply.github.com>
2019-05-09 20:40:01 +0200

    Update v_accueil.php

--- views/v_accueil.php
+++ views/v_accueil.php
@@ -1,26 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-    <link style="css">
+    <meta charset="utf-8">
+    <link style="css" href="<?php echo base_url('css/style.css');?>" rel="stylesheet">
 </head>
 <body>
-	<h2>Accueil</h2>
-	<img src="./../images/la_criee.jpg" width="50%">
-	<p>Bonjour et bienvenue sur notre boutique en ligne. Vous pourrez y découvrir la sélection de nos poissons dans notre catalogue. Nous vous invitons à vous inscrire et à profiter de nos articles. Veuillez nous contacter pour plus d'informations.</p>
+    <img src="./../images/la_criee.jpg" width="50%" align="left">
     <table align="center">
-        <td><a href="<?php echo site_url('utilisateur/contenu/catalogue');?>"><img src="./../images/nos_produits.png" align="left"></a></td>
-        <td><a href="<?php echo site_url('utilisateur/contenu/enchere');?>"><img src="./../images/img_encheres.jpg" align="right"></a></td>
-    </table>
-    <h1>Qu'est-ce que la Criée de Cournaille ?</h1>
-    <br>
-    <p>La Criée de Cournaille est une vente aux enchères d'une marchandise par lots. situées dans la commune de Plouhinec, département du Finistère (29), à proximité de la Pointe du Razsur la zone de pêche Atlantique du Nord Est.</p>
+        <tr><td><a href="<?php echo site_url('utilisateur/contenu/catalogue');?>"><img src="./../images/nos_produits.png" align="left"/></a></td></tr>
+        <tr><td><a href="<?php echo site_url('utilisateur/contenu/enchere');?>"><img src="./../images/img_encheres.jpg" align="right"></a></td></tr></table>
+    <img src="./../images/poisson_avril.jpg">
+    <p>Bonjour et bienvenue sur notre boutique en ligne. Vous pourrez y découvrir la sélection de nos poissons dans notre catalogue. Nous vous invitons à vous inscrire et à profiter de nos articles. Veuillez nous contacter pour plus d'informations.</p>
+    <h2>Qui sommes nous ?</h2>
+
+    <p>La Criée de Cornouaille est une vente aux enchères d'une marchandise par lots. Notre entreprise est située dans la commune de Plouhinec, département du Finistère (29), à proximité de la Pointe du Razsur la zone de pêche Atlantique du Nord Est.</p>
     <table>
         <td><img src="./../images/localisation_criee.jpg" width="60%"></td>
         <td><img src="./../images/loc_criee.jpg" width="90%"></td>
     </table>
-    
-    <p>La marchandise vendue est le produit de la pêche, c'est-à-dire des poissons. Les vendeurs sont aussi des pêcheurs, les acheteurs sont en général des marreyeurs, mais ils peuvent être également des représentants de la grande distribution, des restaurateurs...</p>
-    <p>Les enchères descendent tant qu'aucun acheteur n'est preneur de la marchandise. Dans le cas contraire, le lot est cédé à une entreprise d'équarrissage.</p>
+    <p>La marchandise vendue est le produit de la pêche, c'est-à-dire des poissons. Les vendeurs sont aussi des pêcheurs, mais peuvent être également des représentants de la grande distribution, des restaurateurs etc... <br>Nos clients sont des acheteurs particuliers, des restaurateurs, des poissonniers et même des entreprises. <br>
+    Les enchères descendent tant qu'aucun acheteur n'est preneur de la marchandise. Dans le cas contraire, le lot est cédé à une entreprise d'équarrissage.</p>
+    <h2>Notre objectif</h2>
+    <p>Nous voulons nous assurer que vous trouvez les produits que vous désirez et que vous puissez effectuer vos achats plus facilement en vous permettant des commandes en ligne par transaction bancaire et une livraison rapide directement à votre domicile.</p>
+    <h2>Moyens de paiement sécurisées</h2>
+    <p>Les cartes bancaires suivantes sont acceptées sur notre site : Carte Bleue, Visa, Visa Electron, MasterCard, American Express. <br>
+Le paiement par carte bancaire est intégralement sécurisé. <br><br> Paypal offre la possibilité de payer par carte bancaire ou virement bancaire AVEC ou SANS compte paypal. Pour éviter tout risque de détournement de votre carte bancaire, nous utilisons un système de paiement sécurisé aux normes SSL, le protocole de cryptage le plus sûr et le plus répandu actuellement. Vos coordonnées bancaires sont directement cryptées et routées vers Paypal sans passer par notre site. <br>
+Cela signifie qu’aucune information bancaire ne nous est transmise. Votre numéro de carte ne sera jamais connu ni conservé. <br><br> Pour toute commande passée sur le web ou par téléphone, vous disposez de 21 jours à compter de la date de votre commande pour changer d'avis.
+Quelle que soit la raison, vous pouvez nous retourner un article ou annuler votre commande.</p>
+<h2>Livraison</h2>
+<p>Les délais de livraison varient en fonction du type de produit commandé, de l'adresse et du mode de livraison. Pendant la période des soldes et des promotions, les délais de livraison peuvent être un peu plus longs que d'habitude. <br><br>Les délais de livraison des commandes passées un jour férié ou la veille peuvent être plus longs. Cela concerne les dates suivantes: 01.01.2019, 19.04.2019, 21.04.2019, 22.04.2019, 01.05.2019, 08.05.2019, 30.05.2019, 09.06.2019, 10.06.2019, 14.07.2019, 15.08.2019, 01.11.2019, 25.12.2019, 26.12.2019. <br><br>Les commandes sont expédiées uniquement pendant les jours ouvrables. Les jours ouvrables sont du lundi au vendredi, à l’exception des jours fériés en France et en Allemagne. En raison des difficultés logistiques liées à la livraison dans certaines zones isolées, nous nous réservons le droit d'annuler votre commande.</p>
 </body>
 </html>
