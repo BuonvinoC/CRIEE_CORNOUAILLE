96757e8b68849c173ee3553ae207d6cb2f2bbd62
MDeghaud67 <43568436+MDeghaud67@users.noreply.github.com>
2019-05-09 20:39:20 +0200

    Add files via upload

---
+++ views/v_faq.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Foire aux questions</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('css/style.css')?>">
+</head>
+<body>
+	<h2>Je ne peux pas me connecter à la boutique en ligne. Que faire?</h2>
+	<p>Avez-vous saisi l’adresse e-mail et le mot de passe corrects? Si vous avez oublié votre mot de passe, demandez en un nouveau.</p>
+	<h2>Le paiement en ligne est-il sécurisé?</h2>
+	<p>Nous attachons une grande importance à la sécurité lors des transactions. Vos données sont transmises uniquement sous forme cryptée, de sorte que des tiers ne puissent pas les lire. Votre paiement par carte de crédit est traité par un partenaire certifié en matière de sécurité</p>
+	<h2>Comment puis-je annuler ma commande?</h2>
+	<p>Vous pouvez annuler votre commande en faisant usage de votre droit de révocation légal. Il suffit de nous envoyer un e-mail ou, si la marchandise a déjà été expédiée, de nous la retourner directement en refusant de la réceptionner.</p>
+	<h2>Comment contacter Criée de Cornouailles à propos d'autres sujets?</h2>
+	<p>Dans notre boutique en ligne, cliquez sur le lien « Contact ». Via le formulaire de contact, vous pouvez poser vos questions à notre équipe du Service clientèle. Nous transfèrerons votre question directement au bon interlocuteur.</p>
+</body>
+</html>
