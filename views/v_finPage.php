4cb5e72596953c832123d3021bfec9136a539cfe
MDeghaud67 <43568436+MDeghaud67@users.noreply.github.com>
2019-05-10 16:12:40 +0200

    Update v_finPage.php

--- views/v_finPage.php
+++ views/v_finPage.php
@@ -12,9 +12,9 @@
         <td><img src="<?php echo base_url('/images/livraison48.jpg');?>" width="50%" align="left"></td>
     </table>
     <table align="center">
-        <td><a href="">Contact</a></td>
-        <td><a href="">Mentions légales</a></td>
-        <td><a href="">FAQ</a></td>
+        <td><a href="<?php echo site_url('utilisateur/contenu/contact');?>">Contact</a></td>
+        <td><a href="<?php echo site_url('utilisateur/contenu/mentions');?>">Mentions légales</a></td>
+        <td><a href="<?php echo site_url('utilisateur/contenu/faq');?>">FAQ</a></td>
     </table>
 <p><a href="">Copyright...</a></p>
 </div>
