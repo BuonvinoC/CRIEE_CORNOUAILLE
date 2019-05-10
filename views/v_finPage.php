96e307d13c540623b6abdc1dd0c53444a96d9f7a
MDeghaud67 <43568436+MDeghaud67@users.noreply.github.com>
2019-05-10 16:57:13 +0200

    Update v_finPage.php

--- views/v_finPage.php
+++ views/v_finPage.php
@@ -10,6 +10,7 @@
         Fax: 02.98.74.93.31</td>
         <td><img src="<?php echo base_url('/images/paiement.png');?>" width="50%" align="right"></td>
         <td><img src="<?php echo base_url('/images/livraison48.jpg');?>" width="50%" align="left"></td>
+        <td><img src="./../images/poisson_avril.jpg" width="80%"></td>
     </table>
     <table align="center">
         <td><a href="<?php echo site_url('utilisateur/contenu/contact');?>">Contact</a></td>
