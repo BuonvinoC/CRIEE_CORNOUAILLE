128b377c1afd13eb82d36df8d8ea33ab33dc384a
MDeghaud67 <43568436+MDeghaud67@users.noreply.github.com>
2019-05-18 19:12:57 +0200

    Update v_finPage.php

--- views/v_finPage.php
+++ views/v_finPage.php
@@ -5,9 +5,9 @@
 <div id="finPage">
    
     <table align="center">
-        <td>Adresse: Terre plein de Plougoazec, 29780, PLOUHINEC, FRANCE<br>
-        Tel: 02.98.70.87.33<br>
-        Fax: 02.98.74.93.31</td>
+        <td><img src="<?php echo base_url('/images/adresse.png');?>" width="5%"> : Terre plein de Plougoazec, 29780, PLOUHINEC, FRANCE<br>
+        <img src="<?php echo base_url('/images/tel.png');?>" width="5%"> : 02.98.70.87.33<br>
+        <img src="<?php echo base_url('/images/fax.png');?>" width="5%"> : 02.98.74.93.31</td>
         <td><img src="<?php echo base_url('/images/paiement.png');?>" width="80%" align="right"></td>
         <td><img src="<?php echo base_url('/images/livraison48.jpg');?>" width="80%" align="left"></td>
         <td><img src="<?php echo base_url('/images/poisson_avril.jpg');?>" width="80%"></td>
