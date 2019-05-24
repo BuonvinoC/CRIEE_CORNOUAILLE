4a77518f3d2611a2c46ecedf3e2a3e4b785b4736
Zimsacha <43068491+Zimsacha@users.noreply.github.com>
2019-05-24 17:14:48 +0200

    Update Utilisateur.php

--- controllers/Utilisateur.php
+++ controllers/Utilisateur.php
@@ -4,41 +4,36 @@ class Utilisateur extends CI_Controller {
 	public function contenu($id){
 		$this->load->helper('url_helper');
 		$this->load->view('v_entete');
-		$data['Utilisateur'] = $this;
-		$this->method_call =& get_instance();
+                $this->load->view('v_bandeau');
+		//$data['Utilisateur'] = $this;
+		//$this->method_call =& get_instance();
 	switch ($id) {
 		case 'catalogue':
 			$this->load->model('main_model');
 			$data['donnees']=$this->main_model->afficheProduits1();
-			//$this->encherir($dataConnect);
-			$this->load->view('v_bandeau');
 			$this->load->view('v_catalogue',$data);
 			break;
 		case 'enchere':
 			$this->load->model('main_model');
 			$data['donnees']=$this->main_model->afficheProduits2();
-			//$this->encherir($dataConnect);
-			$this->load->view('v_bandeau');
+
 			$this->load->view('v_enchere',$data);
 			break;
 			case 'remporte':
 				$this->load->model('main_model');
 				$data['donnees']=$this->main_model->afficheLotRemporte();
-				$this->load->view('v_bandeau');
+				//$this->load->view('v_bandeau');
 				$this->load->view('v_remporte',$data);
 				break;
 		case 'admin':
 		$this->load->model('main_model');
 		$data['donnees']=$this->main_model->afficheLotPropose();
-			$this->load->view('v_bandeau');
 			$this->load->view('v_admin',$data);
 			break;
 		case 'inscription':
-			$this->load->view('v_bandeau');
 			$this->load->view('v_inscription');
 			break;
 		case 'connexion':
-			$this->load->view('v_bandeau');
 			$this->load->view('v_connexion');
 			break;
 		case 'deconnexion':
@@ -48,18 +43,26 @@ class Utilisateur extends CI_Controller {
 					'logged_in' => FALSE
 				);
 			$this->session->set_userdata($sessionData);
-			$this->load->view('v_bandeau');
 			session_destroy();
 			break;
 		case 'propose':
-			$this->load->view('v_bandeau');
 			$this->load->view('v_propose');
 			break;
+                case 'contact':
+			$this->load->view('v_contact');
+			break;
+		case 'mentions':
+			$this->load->view('v_mentions');
+			break;
+		case 'faq':
+			$this->load->view('v_faq');
+			break;
+
 		default:
-			$this->load->view('v_bandeau');
 			$this->load->view('v_accueil');
 			}
 	$this->load->view('v_finPage');
+
 		}
 	public function ajout_utilisateur() {
   /**Chargement des méthodes si déclarées dans le contrôleur**/
@@ -71,16 +74,21 @@ class Utilisateur extends CI_Controller {
 		'prenom' => $this->input->post('prenomClient'),
 		'mail' => $this->input->post('mailClient'),
 		'pwd' => $this->input->post('mdpClient'),
+		'type' => $this->input->post('typeClient')
 		);
+		
 	  /*$this->load->model('main_model');
 	  $this->main_model->InsertClient($nom,$prenom,$mail);*/
 	  $this->load->model('main_model');
 	  $this->main_model->InsertClient($data);
+
+          //header('Location: http://[::1]/CodeIgniter-3.1.9_Criee/index.php/utilisateur/contenu/connexion');
+		exit();
 	  /*$this->load->database();
 	  $sql = $this->db->conn_id->prepare("INSERT INTO client(nomClient, prenomClient, mailClient) VALUES ($nom,$prenom,$mail);");
     $sql->execute();*/
 		}
-	public function ajout_commentaire() {
+	/*public function ajout_commentaire() {
 	$chex = array ('idLot'=>$this->input->post('chbx'),
 	'mail'=>$this->session->userdata('login'));
 	var_dump($chex);
@@ -90,13 +98,17 @@ class Utilisateur extends CI_Controller {
 	  $this->main_model->InsertPanierIntermediaire($chex);
 		$this->load->view('v_bandeau');
 		$this->load->view('v_commentaire');
-		}
+		}*/
+
+
 	public function connexion_utilisateur () {
 		$dataConnect = array ('mail' => $this->input->post('mailClient'),
 		'pwd' => $this->input->post('mdpClient'),
 		);
 		$this->load->model('main_model');
 		$this->main_model->connexionClient($dataConnect);
+                /*header('Location: http://[::1]/CodeIgniter-3.1.9_Criee/index.php/utilisateur/contenu/catalogue');
+		exit();*/
 	}
 	public function encherir () {
                 $data = array ('prix_propose' => $this->input->post('ajoutMontant'), 'utilisateur' => $this->session->userdata('login'), 'idLot' =>$this->input->post('idL'));
@@ -116,16 +128,20 @@ class Utilisateur extends CI_Controller {
 		exit();
 		}
         public function valider_lot() {
-                $data = array ('prix' => $this->input->post('prixLot'),
+                $data = array ('idLot' => $this->input->post('idL'),
+                'prix' => $this->input->post('prixLot'),
                 'dateFin' => $this->input->post('dateFinEnchere'),
                 'libel' => $this->input->post('lbl'),
                 'dat' => $this->input->post('datePeche'),
-								'poids' => $this->input->post('poids')
+                'poids' => $this->input->post('poids')
                 );
                 $this->load->model('main_model');
-                $this->main_model->InsertLot($data['prix'], $data['dateFin'], $data['libel'], $data['dat'], $data['poids']);
-								header('Location: http://[::1]/CodeIgniter-3.1.9_Criee/index.php/utilisateur/contenu/enchere');
-  	  			exit();
+                $this->main_model->InsertLot($data['idLot'],$data['prix'], $data['dateFin'], $data['libel'], $data['dat'], $data['poids']);
+                /*$this->load->model('main_model');
+                $this->main_model->DeleteLotPropose($data['idLot']);*/
+
+                header('Location: http://[::1]/CodeIgniter-3.1.9_Criee/index.php/utilisateur/contenu/enchere');
+                exit();
                 }
         public function finEnchere()
 	{
@@ -138,6 +154,17 @@ class Utilisateur extends CI_Controller {
 	header('Location: http://[::1]/CodeIgniter-3.1.9_Criee/index.php/utilisateur/contenu/enchere');
   	  exit();
 	}
+
+        	public function choisirLot()
+	{
+		  $LesLotsChoisis = $this->input->post('choixLots[]');
+		  $this->load->model('main_model');
+		 $this->main_model->ajoutPanierTemporaire($LesLotsChoisis);
+                 		header('Location: http://[::1]/CodeIgniter-3.1.9_Criee/index.php/utilisateur/contenu/enchere');
+  	 	 exit();
+	}
+
+
 	public function index()
 	{
 		$this->load->helper('url_helper');
